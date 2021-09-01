b5d17f825b10a59a6f6487069589dc2e46416cba
Aryan <aryanpathak0123@gmail.com>
2021-09-01 16:35:09 +0530

    added optimized solution

--- Algorithm Problems/rotatearray.cpp
+++ Algorithm Problems/rotatearray.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 using namespace std;
 #include <vector>
-void rotate(vector<int> nums, int k)
+//optimized solution
+void swap(vector<int>& nums,int start, int end){
+    while(start<=end){
+        int temp=nums[start];
+        nums[start]=nums[end];
+        nums[end]=temp;
+        start++;
+        end--;
+    }
+    
+}
+
+void rotate1(vector<int>& nums, int k)     //Optimized solution for for any no. of input 
+{
+    int size = nums.size();
+    k=k%size;
+
+    swap(nums,0,size-1);
+    swap(nums,0,k-1);
+    swap(nums,k,size-1);
+}
+
+//unoptimzed solution;  timeout error possible and suittable for less range of input
+void rotate(vector<int>& nums, int k)    
 {
     int size = nums.size();
     int j = 0;
@@ -10,17 +33,11 @@ void rotate(vector<int> nums, int k)
         int first = nums[0];
         for (int i = 1; i <size; i++)
         {   
-            int c=nums[i];
-            cout<<c;
             nums[i-1]=nums[i];
-            cout<<"updated"<<nums[i-1]<<endl;
         }
         nums[size-1]=first;
-        cout<<"next"<<nums[size-1]<<endl;
         j++;
-// cout<<k;
     }
-    // cout<<size<<endl;
     for (int i = 0; i <size; i++)
     {
         cout << nums[i] << ' ';
@@ -43,7 +60,12 @@ int main()
         nums.push_back(val);
         cout << endl;
     }
-    // cout<<nums.size();
-    rotate(nums, k);
+    // rotate(nums, k);
+    rotate1(nums,k);
+
+    for (int i = 0; i <nums.size(); i++)
+    {
+        cout << nums[i] << ' ';
+    }
 
 }
